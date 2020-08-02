6f0d14a2c3384599e65cd0909b337ba70a99dc62
yzuxqz <1336352883@qq.com>
2020-08-02 10:51:15 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
+// 按需导入
 import Vue from 'vue'
-import { Button } from 'element-ui'
-
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+// 注册为全局可用组件
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 全局挂载消息弹框组件
+Vue.prototype.$message = Message
